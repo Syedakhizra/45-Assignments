381d206b8b29437f673ab868119295772c4f93d3
Syedakhizra <syedakhizra000@gmail.com>
2024-05-03 22:19:51 +0500

    ninth question

---
+++ Question 9/2ndsub9.js
@@ -0,0 +1,5 @@
+// It's my second comment
+// I am Syeda Khizra
+// Dated:24/2/2024
+console.log("It's my second simplest program");
+export {};
---
+++ Question 9/2ndsub9.ts
@@ -0,0 +1,4 @@
+// It's my second comment
+// I am Syeda Khizra
+// Dated:24/2/2024
+console.log("It's my second simplest program");
