6a099a904147813306681ccc6500d19107c3d9c1
Syedakhizra <syedakhizra000@gmail.com>
2024-05-05 21:31:00 +0500

    tenth question

---
+++ Question 10/sub10.js
@@ -0,0 +1,6 @@
+// arrays and loop
+let friendsName = ["fiza", "hania", "alishba", "ifra", "zobia"];
+for (let i = 0; i > friendsName.length; i++) {
+    console.log(friendsName[i]);
+}
+export {};
---
+++ Question 10/sub10.ts
@@ -0,0 +1,6 @@
+// arrays and loop
+
+let friendsName: string[] = ["fiza","hania","alishba","ifra","zobia"];
+for(let i=0; i>friendsName.length; i++){
+    console.log(friendsName[i]);
+}
