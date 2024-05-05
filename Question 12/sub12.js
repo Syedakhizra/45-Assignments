8cd3e5c7f609488bc905fc044e350fabb6cfc44d
Syedakhizra <syedakhizra000@gmail.com>
2024-05-05 22:15:38 +0500

    twelveth question

---
+++ Question 12/sub12.js
@@ -0,0 +1,6 @@
+// It's my own array
+let transportation = ["civic", "bike", "ship", "aeroplane"];
+for (let i = 0; i < transportation.length; i++) {
+    console.log(`i would like to own a ` + transportation[i]);
+}
+export {};
---
+++ Question 12/sub12.ts
@@ -0,0 +1,5 @@
+// It's my own array
+let transportation: string[] = ["civic", "bike", "ship", "aeroplane"];
+for(let i=0; i<transportation.length; i++){
+    console.log(`i would like to own a ` + transportation[i]);
+}
