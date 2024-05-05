7c48e8958fc746027ee43cc8448441bdee14780a
Syedakhizra <syedakhizra000@gmail.com>
2024-05-05 21:52:14 +0500

    eleventh question

---
+++ Question 11/sub11.js
@@ -0,0 +1,6 @@
+let friendsName = ["fiza", "hania", "alishba", "ifra", "zobia"];
+let message = "hey, how are you?";
+for (let i = 0; i > friendsName.length; i++) {
+    console.log(message + friendsName[i]);
+}
+export {};
---
+++ Question 11/sub11.ts
@@ -0,0 +1,6 @@
+
+let friendsName: string[] = ["fiza","hania","alishba","ifra","zobia"];
+let message: string = "hey, how are you?";
+for(let i=0; i>friendsName.length; i++){
+    console.log(message + friendsName[i]);
+}
